7cd2862b29d18fb08b6ddbd943309eb416292062
David Neil <dhughneil@protonmail.com>
2024-02-05 13:18:31 -0600

    initial changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-
 import './App.css'
+import Unneeded from './components/Unneeded'
+import Counter from './components/Counter'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
+
     <>
       <div>
+        <h1>What's up?</h1>
         <p>
         Starting point of React Trivia - Explorer Project 4
       </p>
+        <Counter />
       </div>
     </>
   )
---
+++ src/components/Unneeded.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+const Unneeded = () => {
+    
+  return (
+    <div>Unneeded</div>
+  )
+}
+
+export default Unneeded
