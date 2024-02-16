a820f6170a6f567b96b7dd5bb049bc8ff0610ed2
David Neil <dhughneil@protonmail.com>
2024-02-15 20:03:58 -0600

    Added required additional questions

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -65,10 +65,64 @@ const questions = [
   },  
   {
     id: 11,
-    text: "Game Over",
-    options: [ "Game Over", "Game Over", "Game Over", "Game Over"],
-    answer: "Game Over",
-  } 
+    text: "What is the capital of The United States of America",
+    options: [ "Washington D.C.", "New York", "Chicago", "Boston"],
+    answer: "Washington D.C.",
+  },
+  {
+    id: 12,
+    text: "What is the capital of Canada?",
+    options: [ "Toronto", "Montreal", "Ottawa", "Vancouver"],
+    answer: "Ottawa",
+  }, 
+  {
+    id: 13,
+    text: "What is the capital of Belize?",
+    options: [ "Belmopan", "Belize City", "Lamnanai", "Piedras Negras"],
+    answer: "Belmopan",
+  }, 
+  {
+    id: 14,
+    text: "What is the capital of Egypt?",
+    options: [ "Alexandria", "Jerusalem", "Khartoum", "Cairo"],
+    answer: "Cairo",
+  }, 
+  {
+    id: 15,
+    text: "What is the capital of Republic of Ireland?",
+    options: [ "Belfast", "Dublin", "Cork", "Derry"],
+    answer: "Dublin",
+  }, 
+  {
+    id: 16,
+    text: "What is the capital of Northern Ireland?",
+    options: [ "Belfast", "Dublin", "Cork", "Derry"],
+    answer: "Belfast",
+  }, 
+  {
+    id: 17,
+    text: "What is the capital of Singapore?",
+    options: [ "Taiwan", "Singapore", "Beijing", "Kuala Lumpur"],
+    answer: "Singapore",
+  }, 
+  {
+    id: 18,
+    text: "What is the capital of India?",
+    options: [ "Mumbai", "Bombay", "Punjab", "New Delhi"],
+    answer: "New Delhi",
+  }, 
+  {
+    id: 19,
+    text: "What is the capital of Sweden?",
+    options: [ "Stockholm", "Oslo", "Gothenburg", "Fyllinge"],
+    answer: "Stockholm",
+  }, 
+  {
+    id: 20,
+    text: "What is the capital of Columbia?",
+    options: [ "Cartagena", "Caracas", "Bogota", "Medellin"],
+    answer: "Bogota",
+  }
 ];
 
 function Questions({updateScore, endGame}) {
@@ -90,7 +144,7 @@ function Questions({updateScore, endGame}) {
     updateScore(currentAnswer)
     setCurrentAnswer(false)
     setCurrentQuestionIndex(currentQuestionIndex + 1);
-    (questionsLeft === 1) ? endGame(true) :
+    (questionsLeft === 0) ? endGame(true) :
     setOptionsSelected(false)
   }
 
@@ -107,7 +161,7 @@ function Questions({updateScore, endGame}) {
     <>
 
     <div>
-    <p style={{ color: 'green' }}>Question {currentQuestionIndex + 1} of {questions.length - 1}</p>
+    <p style={{ color: 'green' }}>Question {currentQuestionIndex + 1} of {questions.length}</p>
     <h3 style={{ color: 'darkblue'}}>{currentQuestion.text}</h3>
     <div><br></br></div>
     
